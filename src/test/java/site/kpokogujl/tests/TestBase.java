bfd9503b396cb66bb037bd21724f0031531535ac
Chikov Sergey <chekistne@mail.ru>
2022-04-11 18:06:44 +0300

    Returned Allure listener

--- src/test/java/site/kpokogujl/tests/TestBase.java
+++ src/test/java/site/kpokogujl/tests/TestBase.java
@@ -15,6 +15,8 @@ import static com.codeborne.selenide.Selenide.closeWebDriver;
 public class TestBase {
     @BeforeAll
     static void setUp(){
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
         TestsConfig config = ConfigFactory.create(TestsConfig.class, System.getProperties());
 
         String browserName = String.valueOf(config.getBrowser());
